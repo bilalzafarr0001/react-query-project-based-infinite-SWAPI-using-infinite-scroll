b0abd5ec81ca45644e08b37cdd12d0be655b9787
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2023-01-28 00:57:35 +0500

    Showing Person and Species as both on pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
     <div className="App">
       <h1>Infinite SWAPI</h1>
-      {/* <InfinitePeople /> */}
+      <InfinitePeople />
       <InfiniteSpecies />
       <ReactQueryDevtools />
     </div>
